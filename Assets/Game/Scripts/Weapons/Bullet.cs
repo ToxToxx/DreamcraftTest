2b0030853d2a253c66ed0c790e845dd671d41b48
Matvey <sinema3d00@gmail.com>
2025-08-29 22:13:21 +0300

    delete not needed logs

--- Assets/Game/Scripts/Weapons/Bullet.cs
+++ Assets/Game/Scripts/Weapons/Bullet.cs
@@ -23,7 +23,6 @@ namespace Game.Gameplay.Weapons
             _onDone = onDone;
             _rb.linearVelocity = vel;
             _prevPos = transform.position;
-            Debug.Log($"[BULLET] Launch vel={vel} life={life} dmg={damage} name={name}");
         }
 
         void Update()
@@ -37,10 +36,8 @@ namespace Game.Gameplay.Weapons
                 if (Physics.SphereCast(_prevPos, radius, delta.normalized, out var hit, dist, _hitMask, QueryTriggerInteraction.Collide))
                 {
                     var e = hit.collider.GetComponent<Game.Gameplay.Enemies.EnemyBase>() ?? hit.collider.GetComponentInParent<Game.Gameplay.Enemies.EnemyBase>();
-                    Debug.Log($"[BULLET] RayHit other={hit.collider.name} enemy={(e != null)} point={hit.point}");
                     if (e != null)
                     {
-                        Debug.Log($"[BULLET] ApplyDamage enemy={e.name} dmg={_damage}");
                         e.TakeDamage(_damage);
                         _onDone?.Invoke(this);
                         return;
@@ -52,18 +49,15 @@ namespace Game.Gameplay.Weapons
             _life += Time.deltaTime;
             if (_life >= _lifeMax)
             {
-                Debug.Log($"[BULLET] Timeout despawn name={name}");
                 _onDone?.Invoke(this);
             }
         }
 
         void OnTriggerEnter(Collider other)
         {
-            Debug.Log($"[BULLET] TriggerEnter other={other.name} layer={LayerMask.LayerToName(other.gameObject.layer)} self={name}");
             var e = other.GetComponent<Game.Gameplay.Enemies.EnemyBase>() ?? other.GetComponentInParent<Game.Gameplay.Enemies.EnemyBase>();
             if (e != null)
             {
-                Debug.Log($"[BULLET] Hit enemy={e.name} dmg={_damage}");
                 e.TakeDamage(_damage);
                 _onDone?.Invoke(this);
             }
@@ -72,7 +66,6 @@ namespace Game.Gameplay.Weapons
         public void OnSpawned()
         {
             _prevPos = transform.position;
-            Debug.Log($"[BULLET] Spawned name={name}");
         }
 
         public void OnDespawned()
@@ -84,7 +77,7 @@ namespace Game.Gameplay.Weapons
             }
             transform.localPosition = Vector3.zero;
             transform.localRotation = Quaternion.identity;
-            Debug.Log($"[BULLET] Despawned name={name}");
+
         }
     }
 }
